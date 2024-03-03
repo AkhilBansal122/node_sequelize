af02382ccff1c1f3d41155f02c355e9a45dc7013
akhilbansal122 <akhilbansal122@gmail.com>
2024-03-03 23:22:19 +0530

    login page setup

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Login from "./components/Login";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login/>} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+// src/components/Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Home</h2>
+      {/* Add content for the home page */}
+    </div>
+  );
+};
+
+export default Home;
